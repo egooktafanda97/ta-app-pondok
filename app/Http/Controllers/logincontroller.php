09cbb6ffa97c4e87bba12682ca8490d9b271a4f7
botvi <younoob07mei@gmail.com>
2023-07-29 00:29:06 +0700

    yopi

---
+++ app/Http/Controllers/logincontroller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class logincontroller extends Controller
+{
+    public function halamanlogin(){
+        return view('login.loginaplikasi');
+    }
+    public function postlogin(Request $request){
+        if(Auth::attempt($request->only('email','password'))){
+            return redirect('/home');
+        }
+        return redirect('/');
+
+
+    }
+    public function logout(){
+        Auth::logout();
+        return redirect('/');
+       
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homecontroller;
+use App\Http\Controllers\logincontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/login',[logincontroller::class,'halamanlogin'])->name('login');
+Route::post('/postlogin',[logincontroller::class,'postlogin'])->name('postlogin');
+Route::get('/logout',[logincontroller::class,'logout'])->name('logout');
+route::group(['middleware' => ['auth']], function(){
+    Route::get('/home',[homecontroller::class,'index'])->name('home');
+
 });
